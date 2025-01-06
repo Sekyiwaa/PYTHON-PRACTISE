815e8e51790d67b890fbccf7325d71a72b161c28
Akua Sekyiwaa Osei-Nkwantabisa <oseinkwantabisaakua@gmail.com>
2025-01-05 20:59:24 -0600

    new code

---
+++ Easy/Median in Sorted Arrays-Meta.py
@@ -0,0 +1,16 @@
+def find_median_sorted_arrays(input):
+    """ 
+    :type input: Dict[str, List[int]]
+    :rtype: float
+    """
+
+    nums1 = input["nums1"]
+    nums2 = input["nums2"]
+
+    merged= sorted(nums1 + nums2)
+    n= len(merged)
+    if n % 2 == 0:
+        return (merged[n//2 - 1] + merged[n//2]) / 2
+    else:
+        return merged[n//2]
+        
