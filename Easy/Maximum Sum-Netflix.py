9dfe100633ef531bfde651df5d430d446bc73fef
Akua Sekyiwaa Osei-Nkwantabisa <oseinkwantabisaakua@gmail.com>
2025-05-04 11:18:32 -0500

    new code

---
+++ Easy/Maximum Sum-Netflix.py
@@ -0,0 +1,12 @@
+def max_subarray_sum(nums):
+    if isinstance(nums, dict) and "nums" in nums:
+        nums = nums["nums"]
+    
+    max_sum = nums[0]
+    current_sum = nums[0]
+
+    for num in nums[1:]:
+        current_sum = max(num, current_sum + num)
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum
